ad634b45bff97a76e2dbc6ef30403f5eab5fbf49
Sahnawaz Hussain <sahnawaz643786@gmail.com>
2025-08-06 08:44:13 +0530

    Implemented chat history using local array and id,concept->spread op,arrray method,

--- src/index.js
+++ src/index.js
@@ -1,29 +1,63 @@
-import express, { response } from 'express';
-import { config } from 'dotenv';
-import {main} from './index1.js'
+import express from "express";
+import { config } from "dotenv";
+import { main } from "./index1.js";
 
-const app=express();
+const app = express();
 
 app.use(express.json());
-config({path:"../.env"});
-const PORT=process.env.PORT;
+config({ path: "../.env" });
+const PORT = process.env.PORT;
 
+/*
+    We will put our user History here.
+    in key value pair
+    key -->user id
+    value -->message array []
+*/
+const chatHistory = {
+  1: [],
+  2: [],
+};
 
-app.get("/",(req,res)=>{
-    res.status(200).json({
-        msg:"Oho bete Generative AI sikh Rahe ho"
-    })
-})
-//Hit GenAi 
-app.post("/chat",async (req,res)=>{
-    const text=req.body.prompt;
-    console.log(text)
-    const answer=await main(text);
+app.get("/", (req, res) => {
+  res.status(200).json({
+    msg: "Oho bete Generative AI sikh Rahe ho",
+  });
+});
 
-    res.send(answer)
-})
+//Hit GenAi
+app.post("/chat", async (req, res) => {
+  const { text, id } = req.body;
+  const id1 = parseInt(id);
 
+  if (!text || !id1) {
+    return res.status(400).json({ error: "text and id are required" });
+  }
 
-app.listen(PORT ,()=>{
-    console.log(`app listen on PORt: ${PORT}`)
-})
+  if (!chatHistory[id1]) {
+    chatHistory[id1] = [];
+  }
+
+  //User history find from storage
+  const History = chatHistory[id1];
+
+  //history+Current
+  const promptMessage = [
+    ...History,
+    {
+      role: "user", //Current text
+      parts: [{ text: text }],
+    },
+  ];
+  console.log(promptMessage);
+  const answer = await main(promptMessage);
+
+  History.push({ role: "user", parts: [{ text: text }] });
+  History.push({ role: "model", parts: [{ text: answer }] });
+
+  res.send(answer);
+});
+
+app.listen(PORT, () => {
+  console.log(`app listen on PORt: ${PORT}`);
+});
